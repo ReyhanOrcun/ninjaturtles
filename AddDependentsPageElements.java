1636be01a2139331a53f1888289242b96707ee9d
spiderman <100785275+ReyhanOrcun@users.noreply.github.com>
2022-07-11 13:02:43 -0400

    Update AddDependentsPageElements.java

--- AddDependentsPageElements.java
+++ AddDependentsPageElements.java
@@ -41,6 +41,34 @@ public class AddDependentsPageElements extends CommonMethods {
 	public List<WebElement> relationshipOptions;
 	
 	
+	@FindBy(xpath = "//*[@id=\"3\"]")
+	public WebElement certificateNo;
+
+	@FindBy(xpath = "//*[@id=\"4\"]")
+	public WebElement registrationDateField;
+
+	@FindBy(xpath = "//*[@id='P442404663_root']")
+	public WebElement registrationDate;
+
+	@FindBy(xpath = "//*[@id='P442404663_root']/div/div/div/div/div[2]/div/div[2]/input")
+	public WebElement registrationDateYear;
+
+	@FindBy(xpath = "//*[@id=\"select-options-fd236a54-3ccb-8f76-3950-3c9c43767444\"]")
+	public WebElement registrationDateYearOptions;
+
+	@FindBy(xpath = "//*[@id='P442404663_root']/div/div/div/div/div[2]/div/div[1]/input")
+	public WebElement registrationDateMonth;
+
+	@FindBy(xpath = "//*[@id='select-options-344de70e-59d7-4593-c4ca-1fcfc68ed38b']")
+	public WebElement registrationDateMonthOptions;
+
+	@FindBy(xpath = "//*[@id='P442404663_table']")
+	public WebElement registrationDateDays;
+
+	@FindBy(xpath = "//button[@class=' btn waves-effect waves-green ']")
+	public WebElement saveMarriageInfo;
+
+	
 
 	public AddDependentsPageElements() {
 		PageFactory.initElements(driver, this);
